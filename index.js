a19373d7d8283a9a9e44b9981849b845a5542421
Karl Amoguis <ktamoguis@gmail.com>
2018-04-07 16:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
-`
+
 var topic = "Let's talk about space";
 
 var conversation = `${topic}`;
